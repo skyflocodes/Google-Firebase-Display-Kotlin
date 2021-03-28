567022611add3aaaac0b88a65e7126d8b5747aa7
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2021-03-28 02:37:52 -0400

    database connected
    player info is added to database

--- app/src/main/java/com/example/teamviewapp/CreatePlayerActivity.kt
+++ app/src/main/java/com/example/teamviewapp/CreatePlayerActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.teamviewapp.databinding.ActivityCreatePlayerBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CreatePlayerActivity : AppCompatActivity() {
 
@@ -21,6 +22,12 @@ class CreatePlayerActivity : AppCompatActivity() {
         binding.viewRosterButton.setOnClickListener {
             if (binding.editTextPersonName.text.toString().isNotBlank() && binding.spinner.selectedItemPosition>0){
                 val player = Player()
+                player.name = binding.editTextPersonName.text.toString()
+                player.appg = binding.spinner.selectedItem.toString().toInt()
+
+                val db = FirebaseFirestore.getInstance().collection("players")
+                player.id = db.document().id
+                db.document(player.id!!).set(player)
 
             } else {
                 Toast.makeText(this,"player name and average PPG required", Toast.LENGTH_LONG).show()
