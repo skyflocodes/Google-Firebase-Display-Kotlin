cc453866586c9322bc95ff51244c6ce5bb26d1f9
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2021-03-28 16:36:27 -0400

    live data is displayed
    live data can be seen on view roster page

--- app/src/main/java/com/example/teamviewapp/ViewRosterActivity.kt
+++ app/src/main/java/com/example/teamviewapp/ViewRosterActivity.kt
@@ -3,7 +3,10 @@ package com.example.teamviewapp;
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.example.teamviewapp.databinding.ActivityViewRosterBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ViewRosterActivity : AppCompatActivity() {
 
@@ -15,6 +18,19 @@ class ViewRosterActivity : AppCompatActivity() {
         val view = binding.root
 
         setContentView(view)
+
+        val db = FirebaseFirestore.getInstance().collection("players")
+        db.get().addOnSuccessListener { documents ->
+            for (document in documents){
+                val player = document.toObject(Player::class.java)
+                val textView = TextView(this)
+                val playerString = player.name + ", APPG: " + player.appg.toString()
+                textView.text = playerString
+                textView.textSize = 20f
+
+                binding.linearLayout.addView(textView)
+            }
+        }
 
         binding.createPlayerButton.setOnClickListener {
             var intent = Intent(this, CreatePlayerActivity::class.java)
